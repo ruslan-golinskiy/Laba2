ba51894267fd226ec7cb1a7d33d095a74f20fb24
Ruslan <ruslanlogin78@gmail.com>
2020-05-19 14:35:11 +0300

    3rd

---
+++ Rus OOP 2.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Rus_OOP_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1;
+            for (double i = -(Math.PI / 2); i <= Math.PI / 2; i += Math.PI / 20)
+            {
+                a = 1 / Math.Tan(i);
+                if (i == 0)
+                {
+                    Console.WriteLine("x = {0}       y = не існує", i);
+                }
+                else
+                {
+                    Console.WriteLine("x = {0}       y = {1}", i, a);
+                }
+            }
+        }
+    }
+}
