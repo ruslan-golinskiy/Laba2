d1e54c75545a628a0d4835be3f66d5eae5368ce0
Ruslan <ruslanlogin78@gmail.com>
2020-05-19 21:24:45 +0300

    7th

---
+++ rus OOP 3.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace rus_OOP_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть рядок");
+            string s = Console.ReadLine();
+            string[] s1 = s.Split();
+            for (int i = 0; i < s1.Length; i += 2)
+            {
+                Console.Write(s1[i] + " ");
+            }
+
+
+
+        
+        }
+    }
+}
+
